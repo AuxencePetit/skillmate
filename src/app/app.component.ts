30560129471b7431bcabf5ccddf122e6dcd335cb
AuxencePetit <auxence.petit26@gmail.com>
2025-01-15 08:22:37 +0100

    landing component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { FooterComponent } from './footer/footer.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule, FooterComponent],
+  imports: [RouterOutlet, ButtonModule, FooterComponent, LandingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
