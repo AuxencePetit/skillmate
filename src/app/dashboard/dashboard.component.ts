0d661ca22244dd2cd2e178b42d55993c0153e8c0
AuxencePetit <auxence.petit26@gmail.com>
2025-01-26 21:22:04 +0100

    dashboard setup

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { SessionService } from '../services/session.service';
 import { Router } from '@angular/router';
-
+import { ButtonModule } from 'primeng/button';
+import { RouterOutlet } from '@angular/router';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
 @Component({
   selector: 'app-dashboard',
+  imports: [CommonModule, ButtonModule, RouterOutlet, IconFieldModule, InputIconModule, InputTextModule],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
-
 })
 export class DashboardComponent implements OnInit {
   userInfo: { idUtilisateur: number; nom: string; prenom: string; email: string } | null = null;
@@ -16,13 +21,15 @@ export class DashboardComponent implements OnInit {
   ngOnInit(): void {
     this.userInfo = this.sessionService.getUserInfo();
     if (!this.userInfo) {
-      this.router.navigate(['/login']); // Redirige si non connecté
+      this.router.navigate(['auth/login']); // Redirige si non connecté
     }
   }
-
+  goToProfile(): void {
+    //this.router.navigate(['profile']);
+  }
   logout(): void {
     this.sessionService.clearSession();
-    this.router.navigate(['/login']);
+    this.router.navigate(['auth/login']);
   }
 }
 
