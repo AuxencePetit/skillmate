f57aae384603fc446e13e9b7fc80c6055b910e8e
AuxencePetit <auxence.petit26@gmail.com>
2025-03-09 10:38:11 +0100

    form add mission ( compts , chief, all mission values)

---
+++ src/app/create-mission/create-mission.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-mission',
+  imports: [],
+  templateUrl: './create-mission.component.html',
+  styleUrl: './create-mission.component.scss'
+})
+export class CreateMissionComponent {
+
+}
