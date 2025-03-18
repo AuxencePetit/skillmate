31b2fca94246d7b4f7aeeca16b48339b7d2ceb97
AuxencePetit <auxence.petit26@gmail.com>
2025-03-18 15:15:27 +0100

    setup chef projet view of mission

--- src/app/mission-chef-projet-view/mission-chef-projet-view.component.ts
+++ src/app/mission-chef-projet-view/mission-chef-projet-view.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+import { Competence } from '../models/personnel.model';
+import { Mission } from '../models/mission.model';
+import { Personnel } from '../models/personnel.model';
+import { PersonnelService } from '../services/personnel.service';
+import { MissionService } from '../services/mission.service';
+import { CompetenceService } from '../services/competence.service';
+
 @Component({
   selector: 'app-mission-chef-projet-view',
   imports: [],
@@ -7,5 +14,46 @@ import { Component } from '@angular/core';
   styleUrl: './mission-chef-projet-view.component.scss'
 })
 export class MissionChefProjetViewComponent {
+  competencesMission!: Competence[];
+  missionUser!: Mission;
+  equipe: Personnel[] = [];
+  allPersonnels: Personnel[] = [];
+  constructor(
+    private personnelsService: PersonnelService,
+    private missionService: MissionService,
+    private competenceService: CompetenceService
+  ) {}
 
+  ngOnInit() {
+    this.competenceService.getCompetences().subscribe((competences: Competence[]) => {
+      this.competencesMission = competences;
+    });
+    this.personnelsService.getPersonnels().subscribe((data: any) => {
+      this.allPersonnels = data;
+      for (let personnel of this.allPersonnels) {
+      this.competenceService.getCompetencesByUserId(personnel.idUtilisateur).subscribe((data: any) => {
+        personnel.competences = data;
+
+      });
+    }
+    });
+
+
+ 
+
+
+}
+  appendPersonelToEquipe(personnel: Personnel) {
+    this.equipe.push(personnel);
+  }
+
+  removePersonnelFromEquipe(personnel: Personnel) {
+    this.equipe = this.equipe.filter((p) => p !== personnel);
+  }
+
+  saveEquipe() {
+    this.missionService.updateMission(this.missionUser).subscribe((data: any) => {
+      console.log(data);
+    });
+  }
 }
