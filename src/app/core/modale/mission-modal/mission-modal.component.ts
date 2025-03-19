b07a0755969232de8348d45f1975b47f85f94612
teoAdjimiViseur <121941556+teoAdjimiViseur@users.noreply.github.com>
2025-03-19 11:28:41 +0100

    Changer statut chef de missions
    Changer statut chef de missions

--- src/app/core/modale/mission-modal/mission-modal.component.ts
+++ src/app/core/modale/mission-modal/mission-modal.component.ts
@@ -1,11 +1,17 @@
 import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { PersonnelService } from '../../../services/personnel.service';
-import { MissionService } from '../../../services/mission.service'; // Importez le service Mission
+import { MissionService } from '../../../services/mission.service';
 import { DialogModule } from 'primeng/dialog';
 import { DropdownModule } from 'primeng/dropdown';
 import { CalendarModule } from 'primeng/calendar';
 import { ButtonModule } from 'primeng/button';
 import { FormsModule } from '@angular/forms';
+
+// Définir un type pour chef
+interface Chef {
+  name: string;
+  code: number;
+}
 
 @Component({
   selector: 'app-mission-modal',
@@ -25,18 +31,18 @@ export class MissionModalComponent implements OnInit {
   @Output() displayModalChange = new EventEmitter<boolean>();
 
   mission = {
-    titre: '', // Chaîne de caractères
-    chef: null, // Peut rester null pour l'instant
-    dateDebut: new Date(), // Initialisez avec une date par défaut
-    duree: 0, // Initialisez avec un nombre par défaut
-    description: '', // Chaîne de caractères
+    titre: '',
+    chef: {} as Chef, // Initialiser objet de type Chef
+    dateDebut: new Date(), // date par défaut
+    duree: 0,
+    description: '',
   };
 
-  chefs: any[] = [];
+  chefs: Chef[] = [];
 
   constructor(
     private personnelService: PersonnelService,
-    private missionService: MissionService // Injectez le service Mission
+    private missionService: MissionService
   ) {}
 
   ngOnInit() {
@@ -46,10 +52,12 @@ export class MissionModalComponent implements OnInit {
   fetchEmployees() {
     this.personnelService.recupererEmployees().subscribe(
       (data) => {
-        this.chefs = data.map((employee: { nom: any; prenom: any; idUtilisateur: any; }) => ({
-          name: `${employee.nom} ${employee.prenom}`,
-          code: employee.idUtilisateur,
-        }));
+        this.chefs = data.map(
+          (employee: { nom: any; prenom: any; idUtilisateur: any }) => ({
+            name: `${employee.nom} ${employee.prenom}`,
+            code: employee.idUtilisateur,
+          })
+        );
       },
       (error) => {
         console.error('Erreur lors de la récupération des employés:', error);
@@ -57,78 +65,47 @@ export class MissionModalComponent implements OnInit {
     );
   }
 
-  // onSubmit() {
-  //   // Vérifiez que les champs sont valides
-  //   if (!this.mission.titre || !this.mission.dateDebut || !this.mission.duree || !this.mission.description) {
-  //     console.error('Tous les champs sont requis');
-  //     return;
-  //   }
-  
-  //   // Formatez la date au format attendu par le backend (YYYY-MM-DD)
-  //   const formattedDate = this.formatDate(this.mission.dateDebut);
-  
-  //   // Assurez-vous que la durée est un nombre
-  //   const duree = Number(this.mission.duree);
-  
-  //   // Créez l'objet missionData avec les types corrects
-  //   const missionData = {
-  //     nom_mission: this.mission.titre,
-  //     description: this.mission.description,
-  //     date_debut: formattedDate, // Doit être une chaîne de caractères
-  //     duree: duree, // Doit être un nombre
-  //   };
-  
-  //   // Envoyez les données au backend
-  //   this.missionService.createMission(missionData).subscribe(
-  //     (response) => {
-  //       console.log('Mission créée avec succès :', response);
-  //       this.closeModal();
-  //     },
-  //     (error) => {
-  //       console.error('Erreur lors de la création de la mission :', error);
-  //     }
-  //   );
-  // }
-
   onSubmit() {
-    // Vérifiez que les champs sont valides
-    if (!this.mission.titre || !this.mission.dateDebut || !this.mission.duree || !this.mission.description) {
+    if (
+      !this.mission.titre ||
+      !this.mission.dateDebut ||
+      !this.mission.duree ||
+      !this.mission.description ||
+      !this.mission.chef
+    ) {
       console.error('Tous les champs sont requis');
       return;
     }
-  
-    // Formatez la date au format attendu par le backend (YYYY-MM-DD)
+
     const formattedDate = this.formatDate(this.mission.dateDebut);
-  
-    // Assurez-vous que la durée est un nombre
+
     const duree = Number(this.mission.duree);
-  
-    // Créez l'objet missionData avec les types corrects
+
     const missionData = {
       nom_mission: this.mission.titre,
       description: this.mission.description,
-      date_debut: formattedDate, // Doit être une chaîne de caractères
-      duree: duree, // Doit être un nombre
+      date_debut: formattedDate,
+      duree: duree,
+      idUtilisateur: this.mission.chef.code,
     };
-  
-    // Envoyez les données au backend
+
+    // Envoyez backend
     this.missionService.createMission(missionData).subscribe(
       (response) => {
         console.log('Mission créée avec succès :', response);
-        this.closeModal(); // Ferme la modale après la création réussie
+        this.closeModal();
       },
       (error) => {
         console.error('Erreur lors de la création de la mission :', error);
       }
     );
   }
-  
-  
+
   // Méthode pour formater la date au format YYYY-MM-DD
   formatDate(date: Date): string {
     const year = date.getFullYear();
-    const month = ('0' + (date.getMonth() + 1)).slice(-2); // Ajoute un zéro devant si nécessaire
-    const day = ('0' + date.getDate()).slice(-2); // Ajoute un zéro devant si nécessaire
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const day = ('0' + date.getDate()).slice(-2);
     return `${year}-${month}-${day}`;
   }
 
